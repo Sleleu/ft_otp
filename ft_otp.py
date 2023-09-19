0794b17f0c38cb35d840ef0bab49f3f0a32fa358
Sebastien LELEU <leleu.sebastien@hotmail.fr>
2023-09-19 12:46:57 +0200

    add master key option and decrypt_key

--- ft_otp.py
+++ ft_otp.py
@@ -12,12 +12,9 @@
 #                                                                              #
 # **************************************************************************** #
 
-import argparse
-import time
+import argparse, time
+import hmac, struct, hashlib
 from cryptography.fernet import Fernet
-import hmac
-import struct
-import hashlib
 
 # X represents the time step in seconds (default value X = 30 seconds) and is a system parameter.
 X = 30
@@ -27,32 +24,53 @@ T0 = 0
 T = (int(time.time()) - T0) // X
 
 def encrypt_key(K: str):
-    fernet_key: bytes = Fernet.generate_key()
-    cipher = Fernet(fernet_key) # Create fernet instance
-    encrypted_key: bytes = cipher.encrypt(K.encode()) # encrypt K in cipher
-    with open("ft_otp.key", "wb") as filekey:
-        filekey.write(encrypted_key)
+	fernet_key: bytes = Fernet.generate_key()
+	cipher = Fernet(fernet_key) # Create fernet instance
+	encrypted_key: bytes = cipher.encrypt(K.encode()) # encrypt K in cipher
+	with open("ft_otp.key", "wb") as filekey:
+		filekey.write(encrypted_key)
+	print("Key was successfully saved in ft_otp.key.")
+	with open("ft_master.key", "wb") as masterkey:
+		masterkey.write(fernet_key)
+	print("Master key was successfully saved in ft_master.key. Use this master key to read ft_otp.key and create TOTP password")
+
+def decrypt_key(K: str, master_key: str):
+	cipher = Fernet(master_key)
+	decrypted_otp_key = cipher.decrypt(K)
+	return (decrypted_otp_key)
 
 def generateTOTP(K: str, T: int):
-    K_bytes = bytes.fromhex(K)
-    msg = struct.pack(">Q", T) # encode in big endian 8 bytes
-    hmac_result = hmac.new(K_bytes, msg, hashlib.sha1).digest() # calcul a HMAC hash with SHA-1
-    selected_byte = hmac_result[19] & 15
-    result = (struct.unpack(">I", hmac_result[selected_byte:selected_byte+4])[0] & 0x7fffffff) % 1000000
-    otp_str = str(result)
-    return (otp_str.zfill(6))
+	K_bytes = bytes.fromhex(K)
+	msg = struct.pack(">Q", T) # encode in big endian 8 bytes
+	hmac_result = hmac.new(K_bytes, msg, hashlib.sha1).digest() # calcul a HMAC hash with SHA-1
+	selected_byte = hmac_result[19] & 15
+	result = (struct.unpack(">I", hmac_result[selected_byte:selected_byte+4])[0] & 0x7fffffff) % 1000000
+	otp_str = str(result)
+	return (otp_str.zfill(6))
 
 def parse_arguments():
-    desc = "A simple TOTP (Time-based One-Time Password) system capable of generating ephemeral passwords from a master key."
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument("-g", "--generate", nargs=1, help="receives as argument a hexadecimal key of at least 64 characters. The program stores this key safely in a file called ft_otp.key, which is encrypted.")
-    parser.add_argument("-k", "--key", nargs=1, help="generates a new temporary password based on the key given as argument and prints it on the standard output.")
-    return (parser.parse_args())
+	desc = "A simple TOTP (Time-based One-Time Password) system capable of generating ephemeral passwords from a master key."
+	parser = argparse.ArgumentParser(description=desc)
+	parser.add_argument("-g", "--generate", nargs=1, help="receives as argument a hexadecimal key of at least 64 characters. The program stores this key safely in a file called ft_otp.key, which is encrypted.")
+	parser.add_argument("-k", "--key", nargs=1, help="generates a new temporary password based on the key given as argument and prints it on the standard output.")
+	parser.add_argument("-m", "--master", nargs=1, help="Use this master key to read ft_otp.key and create TOTP password")
+	return (parser.parse_args())
 
 if __name__ == "__main__":
-    args = parse_arguments()
-    K = "17f019ff2069c3d054706b6348"
-    if args.generate:
-        encrypt_key(K)
-    otp_code = generateTOTP(K, T)
-    print(otp_code)
+	args = parse_arguments()
+	if args.generate:
+		K = args.generate
+		encrypt_key(K)
+	elif args.key:
+		if args.master is None:
+			print("usage: argument '-m', '--master' MASTER_KEY is required to decrypt ft_opt.key")
+			exit(1)
+		with open("ft_otp.key") as file:
+			otp_key = file.read()
+		with open("ft_master.key") as file:
+			master_key = file.read()
+		decrypted_otp_key = decrypt_key(otp_key.encode(), master_key.encode())
+		otp_code = generateTOTP(decrypted_otp_key.decode(), T)
+		print(otp_code)
+
+# test hex 17f019ff2069c3d054706b6348
